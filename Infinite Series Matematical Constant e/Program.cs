d55be9d53720f89ce455982b86f60da158a0f311
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-30 12:26:31 +0500

    calculating the value of mathematical constant e

--- Infinite Series Matematical Constant e/Program.cs
+++ Infinite Series Matematical Constant e/Program.cs
@@ -10,6 +10,15 @@
         // taking user input
         Console.Write("Enter The Number Of Terms To Be Used In The Series:  ");
         number = Convert.ToInt32(Console.ReadLine());
+
+        // calculating the value of mathematical constant e
+        for (int i = 1; i <= number; i++) 
+        {
+            factorial = factorial * i;
+            e = e + 1 / factorial;
+        }
+
+        
     }
     // end of main method
 }
