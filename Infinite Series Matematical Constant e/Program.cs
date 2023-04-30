a93702cf6daec7b63260712ffc8975544333a1b1
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-30 12:25:15 +0500

    declared variables + taking user input

--- Infinite Series Matematical Constant e/Program.cs
+++ Infinite Series Matematical Constant e/Program.cs
@@ -2,7 +2,14 @@
 {
     static void Main(string[] args)
     {
+        // declaring variables
+        int number;  // number of series
+        double e = 1.0;
+        double factorial = 1.0;
 
+        // taking user input
+        Console.Write("Enter The Number Of Terms To Be Used In The Series:  ");
+        number = Convert.ToInt32(Console.ReadLine());
     }
     // end of main method
 }
